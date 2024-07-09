67dcc8551fb774069ee5fdf1d91c8eab72198b97
Anushruti Mahato <anushruti5882@gmail.com>
2024-07-09 09:45:43 +0530

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,16 @@ import { NoticesComponent } from './components/notices/notices.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ChartsComponent } from './components/charts/charts.component';
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ProductsComponent,NoticesComponent,NavbarComponent,ChartsComponent,PieChartComponent],
+  imports: [RouterOutlet,ProductsComponent,NoticesComponent,NavbarComponent,ChartsComponent,PieChartComponent,HomeComponent,
+    AboutComponent,ContactComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
