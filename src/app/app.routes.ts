8fd2a379810001efd7dc2ddb01016aaf267edf30
Anushruti Mahato <anushruti5882@gmail.com>
2024-07-11 14:49:28 +0530

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,21 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { NewProductsComponent } from './components/new-products/new-products.component';
+import { ProductsComponent } from './components/products/products.component';
+import { NoticesComponent } from './components/notices/notices.component';
+import { PieChartComponent } from './components/pie-chart/pie-chart.component';
+import { ChartsComponent } from './components/charts/charts.component';
 
 
-// @NgModule({
-//     imports: [RouterModule.forRoot(routes)],
-//     exports: [RouterModule]
-//   })
 
 export const routes: Routes = [
     {path:'home',component:HomeComponent},
     {path:'about',component:AboutComponent},
     {path:'contact',component:ContactComponent},
+    {path:'charts',component:ChartsComponent},
+    {path:'pieChart',component:PieChartComponent},
+    {path:'notices',component:NoticesComponent},
+    {path:'products',component:ProductsComponent},
+    {path:'newProducts',component:NewProductsComponent},
 ];
---
+++ src/app/components/new-products/new-products.component.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-products.component.html',
+  styleUrl: './new-products.component.css'
+})
+export class NewProductsComponent {
+  products:any
+  constructor(private http:HttpClient){}
+  ngOnInit(){
+    this.http.get("https://www.fakestoreapi.com/products")
+    .subscribe(res=>
+    {
+      console.log()
+      this.products=res
+    }
+    )
+  }
+}
